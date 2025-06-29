1f7a4656ad25bec0d2bf3aba3078bbc3f2afa1c3
juancafernandez18 <juanca.fernandez18@gmail.com>
2025-06-28 23:21:12 -0300

    dejo en 30s el wait

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -24,7 +24,7 @@ public class BasePage {
      * Se inicializa inmediatamente con una instancia dew WebDriverWait utilizando el 'driver' estático
      * WebDriverWait se usa para poner esperas explícitas en los elementos web
      */
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
     /*
      * Configura el WebDriver para Chrome usando WebDriverManager.
