8f98b50f61ca22dbb18c2d6504ca9234c6de4ad3
juancafernandez18 <juanca.fernandez18@gmail.com>
2025-07-02 20:48:17 -0300

    agrego options en modo incognito

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,9 +2,6 @@ package pages;
 
 // Importaciones necesarias
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
